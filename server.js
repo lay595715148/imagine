409aaee954470084d56b7a05b205e6da28b5e0aa
lay595715148 <lay595715148@126.com>
2014-12-23 17:56:53 +0800

    update Logger

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
+/* jshint eqeqeq: false */
+"use strict";
+
 var port = 8808, api_port = 8133;
 var $ = global.$ = global.$ || require('./lib/util/Require');
 var _ = global._ = global._ || {};//备用
 
 //初始化定义模块或类访问空间
-$.define($, ['amqp', 'assert', 'async', 'bower', 'body-parser', 'cookie-parser', 'cookie-session', 'crypto',
-        'express', 'fn.js', 'geddy', 'helmet', 'jade', 'lru-cache', 'http', 'memcache', 'method-override',
-        'mime', 'moment', 'mongodb', 'mysql', 'net', 'node-uuid', 'os', 'os-name', 'path', 'php-express', 
-        'querystring', 'rabbitmq-nodejs-client', 'redis', 'request', 'scribe', 'socket.io', 'solr-client',
-        'syslog', 'underscore', 'url', 'util']);
+$.define($, ['amqp', 'assert', 'async',
+             'bower', 'body-parser',
+             'console-highlight', 'cookie-parser', 'cookie-session', 'crypto',
+             'express',
+             'fn.js',
+             'geddy', 'grunt',
+             'helmet', 'htmlout', 'http',
+             'jade',
+             'lru-cache',
+             'memcache', 'method-override', 'mime', 'moment', 'mongodb', 'mysql',
+             'net', 'node-uuid',
+             'os', 'os-name',
+             'path', 'php-express',
+             'querystring',
+             'rabbitmq-nodejs-client', 'redis', 'request', 'scribe',
+             'socket.io', 'solr-client', 'syslog',
+             'underscore', 'url', 'util']);
 $.define($, [__dirname + '/lib']);
 
-var cgi = new $.server.Cgi();
-cgi.init();
-cgi.set('port', port);
+var cgi = new $.server.Cgi(port);
 cgi.set('api_port', api_port);
+//cgi.init();
 cgi.open();
